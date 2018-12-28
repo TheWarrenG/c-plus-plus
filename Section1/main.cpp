278186099fdd8cfab4ba3046ff3efc259f2ddf27
Warren Gu <warren.gu@berkeley.edu>
2018-12-27 21:52:58 -0800

    Add reading string input exercise. Delete previous code.

--- Section1/main.cpp
+++ Section1/main.cpp
@@ -1,52 +1,16 @@
+#include <string>
 #include <iostream>
-#include "constants.h"
-
-double readHeight();
-double calculateHeight(int time, double initialHeight);
-
-void printHeight(int time, double height);
-void calculateAndPrintHeight(int time, double height);
 
 int main()
 {
-	double initialHeight = readHeight();
-	calculateAndPrintHeight(0, initialHeight);
-	calculateAndPrintHeight(1, initialHeight);
-	calculateAndPrintHeight(2, initialHeight);
-	calculateAndPrintHeight(3, initialHeight);
-	calculateAndPrintHeight(4, initialHeight);
-	calculateAndPrintHeight(5, initialHeight);
+	std::cout << "Enter your full name: ";
+	std::string name;
+	std::getline(std::cin, name);
+
+	std::cout << "Enter your age: ";
+	int age;
+	std::cin >> age;
+
+	std::cout << "You've lived " << static_cast<float>(age) / name.length() << " years for each letter in your name.";
 	return 0;
-}
-
-double readHeight()
-{
-	std::cout << "Enter the height of the tower in meters: ";
-	double input;
-	std::cin >> input;
-	return input;
-}
-
-double calculateHeight(int time, double initialHeight)
-{
-	double heightChange = 0.5 * myConstants::gravity * time * time;
-	double finalHeight = initialHeight - heightChange;
-	return finalHeight;
-}
-
-void printHeight(int time, double height)
-{
-	if (height > 0)
-	{
-		std::cout << "At " << time << " seconds, the ball is at height: " <<
-			height << " meters" << std::endl;
-	}
-	else
-		std::cout << "At " << time << " seconds, the ball is on the ground.";
-}
-
-void calculateAndPrintHeight(int time, double height)
-{
-	double finalHeight = calculateHeight(time, height);
-	printHeight(time, finalHeight);
 }
