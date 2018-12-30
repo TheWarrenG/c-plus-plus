e99c09b0bba7bc0db6e15721ae63ceda5cb17d34
Warren Gu <warren.gu@berkeley.edu>
2018-12-29 17:00:19 -0800

    Add Section 4 quiz non-C++11 solution. Delete previous program.

--- Section1/main.cpp
+++ Section1/main.cpp
@@ -1,16 +1,48 @@
 #include <string>
 #include <iostream>
 
+enum monsterType
+{
+	MONSTER_OGRE,
+	MONSTER_DRAGON,
+	MONSTER_ORC,
+	MONSTER_GIANT_SPIDER,
+	MONSTER_SLIME
+};
+
+struct Monster
+{
+	monsterType type;
+	std::string name;
+	int damage;
+};
+
+void printMonster(Monster monster)
+{
+	std::string type;
+	if (monster.type == MONSTER_OGRE)
+		type = "Ogre";
+	else if (monster.type == MONSTER_DRAGON)
+		type = "Dragon";
+	else if (monster.type == MONSTER_ORC)
+		type = "Orc";
+	else if (monster.type == MONSTER_GIANT_SPIDER)
+		type = "Giant Spider";
+	else if (monster.type == MONSTER_SLIME)
+		type = "Slime";
+	else
+		type = "Unknown";
+	std::cout << "This " << type << " is named " <<
+		monster.name << " and has " << monster.damage << " health.\n";
+}
+
 int main()
 {
-	std::cout << "Enter your full name: ";
-	std::string name;
-	std::getline(std::cin, name);
+	Monster ogre = { MONSTER_OGRE, "Torg", 145 };
+	Monster slime = { MONSTER_SLIME, "Blurp", 23 };
 
-	std::cout << "Enter your age: ";
-	int age;
-	std::cin >> age;
-
-	std::cout << "You've lived " << static_cast<float>(age) / name.length() << " years for each letter in your name.";
+	printMonster(ogre);
+	printMonster(slime);
+	
 	return 0;
 }
