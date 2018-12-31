d3b66d1ab9d86fe1f0f8925b0d23bedeeb15242c
Warren Gu <warren.gu@berkeley.edu>
2018-12-31 10:17:09 -0800

    Add error-handling to user guess.

--- Hi-Lo/Hi-Lo.cpp
+++ Hi-Lo/Hi-Lo.cpp
@@ -13,18 +13,28 @@ int generateNumber()
 	// Create random number generator that is uniformly distributed from 0 to 100.
 	std::uniform_int_distribution<> gen(0, 100);
 
-	// gen(mersenne);
-
 	return gen(mersenne);
 }
 
 /* Reads in guess number I from the user.*/
 int readGuess(int i)
 {
-	std::cout << "Guess #" << i << ": ";
-	int x;
-	std::cin >> x;
-	return x;
+	while (true)
+	{
+		std::cout << "Guess #" << i << ": ";
+		int x;
+		std::cin >> x;
+
+		if (std::cin.fail() || x < 0 || x > 100)
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+		else {
+			std::cin.ignore(32767, '\n');
+			return x;
+		}
+	}
 }
 
 /* Reads user input to whether the user would like to play again.*/
@@ -34,20 +44,18 @@ char readPlayAgain()
 	do
 	{
 		std::cout << "Would you like to play again (y/n)? ";
-		char temp;
-		std::cin >> temp;
-		c = temp;
+		std::cin >> c;
 	} while (c != 'y' && c != 'n');
 	return c;
 }
 
+/* Plays a game of Hi-Lo.*/
 void playGame()
 {
-	std::cout << "Let's play a game. I'm thinking of a number. " <<
+	std::cout << "Let's play a game. I'm thinking of a number between 0 and 100. " <<
 		"You have 7 tries to guess what it is.\n";
 	int target = generateNumber();
 	int i(1);
-	int guess;
 	for (; i <= 7; ++i)
 	{
 		int guess = readGuess(i);
