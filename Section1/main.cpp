bdd35e4b751280aab87074b2d43b972152a6aed3
Warren Gu <warren.gu@berkeley.edu>
2019-01-04 20:10:54 -0800

    Add iterative version of binary search. Delete previous code.

--- Section1/main.cpp
+++ Section1/main.cpp
@@ -1,95 +1,47 @@
 #include <iostream>
-#include <string>
-#include <utility>
-#include <array>
 
-enum CardRank
+// array is the array to search over.
+// target is the value we're trying to determine exists or not.
+// min is the index of the lower bounds of the array we're searching.
+// max is the index of the upper bounds of the array we're searching.
+// binarySearch() should return the index of the target element if the target is found, -1 otherwise
+int binarySearch(int *array, int target, int min, int max)
 {
-	RANK_2, RANK_3, RANK_4,
-	RANK_5, RANK_6, RANK_7,
-	RANK_8, RANK_9, RANK_10,
-	RANK_JACK, RANK_QUEEN,
-	RANK_KING, RANK_ACE,
-	MAX_RANKS
-};
-
-enum CardSuit
-{
-	SUIT_CLUBS,
-	SUIT_DIAMONDS,
-	SUIT_HEARTS,
-	SUIT_SPADES,
-	MAX_SUITS
-};
-
-struct Card
-{
-	CardRank rank;
-	CardSuit suit;
-};
-
-void printCard(const Card &card)
-{
-	char rank;
-	switch (card.rank)
+	while (min < max)
 	{
-		case(RANK_2):		rank = '2'; break;
-		case(RANK_3):		rank = '3'; break;
-		case(RANK_4):		rank = '4'; break;
-		case(RANK_5):		rank = '5'; break;
-		case(RANK_6):		rank = '6'; break;
-		case(RANK_7):		rank = '7'; break;
-		case(RANK_8):		rank = '8'; break;
-		case(RANK_9):		rank = '9'; break;
-		case(RANK_10):		rank = 'T'; break;
-		case(RANK_JACK):	rank = 'J'; break;
-		case(RANK_QUEEN):	rank = 'Q'; break;
-		case(RANK_KING):	rank = 'K'; break;
-		case(RANK_ACE):		rank = 'A'; break;
+		int test{ (min + max) / 2 };
+		if (array[test] == target)
+			return test;
+		else if (array[test] > target)
+			max = test;
+		else
+			min = test;
 	}
-
-	char suit;
-	switch (card.suit)
-	{
-		case(SUIT_CLUBS):		suit = 'C';	break;
-		case(SUIT_DIAMONDS):	suit = 'D';	break;
-		case(SUIT_HEARTS):		suit = 'H';	break;
-		case(SUIT_SPADES):		suit = 'S';	break;
-	}
-	std::cout << rank << suit;
-}
-
-void printDeck(const std::array<Card, 52> &deck)
-{
-	for (const auto &card : deck)
-	{
-		printCard(card);
-		std::cout << ' ';
-	}
-	std::cout << '\n';
-}
-
-void swapCard(Card &x, Card &y)
-{
-	Card temp = x;
-	x = y;
-	y = temp;
+	return -1;
 }
 
 int main()
 {
-	std::array<Card, 52> deck;
-	using index_t = std::array<Card, 52>::size_type;
-	index_t deckIndex{ 0 };
-	for (int rankIndex = 0; rankIndex < MAX_RANKS; ++rankIndex)
+	int array[] = { 3, 6, 8, 12, 14, 17, 20, 21, 26, 32, 36, 37, 42, 44, 48 };
+
+	// We're going to test a bunch of values to see if they produce the expected results
+	const int numTestValues = 9;
+	// Here are the test values
+	int testValues[numTestValues] = { 0, 3, 12, 13, 22, 26, 43, 44, 49 };
+	// And here are the expected results for each value
+	int expectedValues[numTestValues] = { -1, 0, 3, -1, -1, 8, -1, 13, -1 };
+
+	// Loop through all of the test values
+	for (int count = 0; count < numTestValues; ++count)
 	{
-		for (int suitIndex = 0; suitIndex < MAX_SUITS; ++suitIndex)
-		{
-			deck[deckIndex] = { static_cast<CardRank>(rankIndex), static_cast<CardSuit>(suitIndex) };
-			++deckIndex;
-		}
+		// See if our test value is in the array
+		int index = binarySearch(array, testValues[count], 0, 14);
+		// If it matches our expected value, then great!
+		if (index == expectedValues[count])
+			std::cout << "test value " << testValues[count] << " passed!\n";
+		else // otherwise, our binarySearch() function must be broken
+			std::cout << "test value " << testValues[count] << " failed.  There's something wrong with your code!\n";
 	}
 
 	return 0;
-
 }
