e5aa0cfe1c4f9e92c83dc958a5fd023b1a17d3fb
Warren Gu <warren.gu@berkeley.edu>
2019-01-14 23:09:03 -0800

    Add toy program to illustrate std::shared_ptr. Delete previous program.

--- Section1/main.cpp
+++ Section1/main.cpp
@@ -4,17 +4,15 @@
 class Resource
 {
 public:
-	std::weak_ptr<Resource> m_ptr; // initially created empty
-
 	Resource() { std::cout << "Resource acquired\n"; }
 	~Resource() { std::cout << "Resource destroyed\n"; }
 };
 
 int main()
 {
-	auto ptr1 = std::make_shared<Resource>();
-
-	ptr1->m_ptr = ptr1; // m_ptr is now sharing the Resource that contains it
+	Resource *res = new Resource;
+	auto ptr1{ std::make_shared<Resource>(*res) };
+	auto ptr2{ ptr1 };
 
 	return 0;
 }
